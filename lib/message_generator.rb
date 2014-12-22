22bb6d0ba9e24f8a3b61709475876b51b609a9e0
Matt Hess <matt@mhess.io>
2014-12-22 10:41:07 -0800

    Fix message generation for 'git ci -v'

--- lib/message_generator.rb
+++ lib/message_generator.rb
@@ -2,8 +2,11 @@ class MessageGenerator
   def generate(original_message:, story_id:)
     unless message_is_tagged?(original_message)
       message_lines = original_message.split("\n")
-      message_body = message_lines.reject(&method(:is_info_line?))
-      message_info = message_lines.select(&method(:is_info_line?))
+
+      split_index = message_lines.find_index(&method(:is_info_line?))
+
+      message_body = message_lines.slice(0, split_index)
+      message_info = message_lines[split_index..-1]
 
       new_message = message_body << ["[##{story_id}]",''] << message_info << ['']
       new_message.join("\n")
--- spec/message_generator_spec.rb
+++ spec/message_generator_spec.rb
@@ -52,4 +52,54 @@ hey I have a tracker story
       TEXT
     end
   end
+
+  context 'whatever git ci -v puts out' do
+    it 'adds the story id without messing everything up' do
+      original_message = <<-TEXT
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# On branch master
+# Changes to be committed:
+# =>new file:   .ruby-version
+#
+# ------------------------ >8 ------------------------
+# Do not touch the line above.
+# Everything below will be removed.
+diff --git a/.ruby-version b/.ruby-version
+new file mode 100644
+index 0000000..eca07e4
+--- /dev/null
++++ b/.ruby-version
+@@ -0,0 +1 @@
++2.1.2
+      TEXT
+
+      expected_message = <<-TEXT
+
+[#12345]
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# On branch master
+# Changes to be committed:
+# =>new file:   .ruby-version
+#
+# ------------------------ >8 ------------------------
+# Do not touch the line above.
+# Everything below will be removed.
+diff --git a/.ruby-version b/.ruby-version
+new file mode 100644
+index 0000000..eca07e4
+--- /dev/null
++++ b/.ruby-version
+@@ -0,0 +1 @@
++2.1.2
+      TEXT
+
+      message = MessageGenerator.new.generate(original_message: original_message, story_id: 12345)
+
+      expect(message).to eq(expected_message)
+    end
+  end
 end
