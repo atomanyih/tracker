b033e449508404722b319894f4204046a283ef07
August Toman-Yih and Matt Hess <pair+atomanyih+mhess@pivotal.io>
2014-12-22 14:17:44 -0800

    Adds installation through 'story' command

--- bin/story.rb
+++ bin/story.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/env ruby
 
-puts ARGV
+require 'tracker_git_hook/installer'
+
+target_dir = Dir.pwd
+hook = <<-BASH
+/usr/bin/env ruby -e "require 'tracker_git_hook/prepare_commit_msg';
+TrackerGitHook::PrepareCommitMsg.new.prepare message_file_path: ARGV[0]" $1
+BASH
+TrackerGitHook::Installer.new(target_dir: target_dir, hook: hook).install
--- lib/tracker_git_hook/prepare_commit_msg.rb
+++ lib/tracker_git_hook/prepare_commit_msg.rb
@@ -4,7 +4,6 @@ module TrackerGitHook
   class PrepareCommitMsg
     def prepare(message_file_path:)
       original_message = File.read(message_file_path)
-      story_id = ENV['TRACKER_STORY_ID']
 
       message = MessageGenerator.new.generate(
         original_message: original_message,
@@ -12,6 +11,12 @@ module TrackerGitHook
       )
 
       File.open(message_file_path, 'w') { |f| f.write(message) }
+    end
+
+    private
+
+    def story_id
+      ENV['TRACKER_STORY_ID']
     end
   end
 end
